98959b8679dbe6a7bbb6cd119fb3ad105d73b679
ronahhh <maskalex44@gmail.com>
2024-08-02 19:18:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('Yay! I'm so happy!');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Oh no! That's okay, we can still be friends.');
+});
